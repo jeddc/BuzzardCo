bed66ba29ffd0ea6dee75628542c0a8aa49ad5d2
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2020-05-11 22:22:53 +1000

    Change logo

--- source/admin/previews.js
+++ source/admin/previews.js
@@ -70,7 +70,7 @@ const NavigationPreview = ({ entry }) => {
     <header className="header-primary">
       <div className="logo">
         <a href="/">
-          <img src="/images/logo.svg" alt="Kaldi"/>
+          <img src="/images/logo.png" alt="BuzzardCo"/>
         </a>
       </div>
       <a className="nav-button">
